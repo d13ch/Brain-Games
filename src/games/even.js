305040b2b593db319ef5470ec31c1043255ebd08
Dmitrii Chernov <dm.a.chernov@gmail.com>
2022-11-08 16:36:30 +0300

    Fix Even

--- src/games/even.js
+++ src/games/even.js
@@ -1,18 +1,15 @@
 import logic from '../index.js';
 import randomNum from '../randomNum.js';
 
-const rules = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-};
-
 const gameData = () => {
+  const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
   const num = randomNum(100, 1);
   let result = '';
 
   if (num % 2 === 0) {
     result = 'yes';
   } else result = 'no';
-  return [num, result];
+  return [rules, num, result];
 };
 
-export default () => logic(rules, gameData);
+export default () => logic(gameData);
